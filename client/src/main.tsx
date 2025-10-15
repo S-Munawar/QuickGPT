61724c43f54387e614582a19eabd131aa7635d11
Shaik Munawar <166964538+S-Munawar@users.noreply.github.com>
2025-10-15 12:11:41 +0530

    Revert "Client sidebar"

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AppContextProvider } from './context/AppContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <AppContextProvider>
+    <BrowserRouter>
       <App />
-    </AppContextProvider>
-  </BrowserRouter>,
+    </BrowserRouter>
 )
